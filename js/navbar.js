3f9daca0c333e68c75a4e141f9bf9827e172448d
vynguyen <vynguyen@umich.edu>
2024-03-24 19:41:52 -0400

    update nav

--- js/navbar.js
+++ js/navbar.js
@@ -1,22 +1,8 @@
-// Dynamically generate the navigation bar
-const navItems = [
-    { text: 'Home', link: 'index.html' },
-    { text: 'Services', link: 'services.html' },
-    { text: 'Gallery', link: 'gallery.html' },
-    { text: 'Contact', link: 'contact.html' },
-    // { text: 'Gift Cards', link: 'gift_cards.html'}
-];
-
-const navbar = document.getElementById('navbar');
-
-navItems.forEach(item => {
-    const link = document.createElement('a');
-    link.href = item.link;
-    link.textContent = item.text;
-    navbar.appendChild(link);
-
-    // Highlight the current tab based on the page URL
-    if (window.location.href.includes(item.link)) {
-        link.classList.add('current');
-    }
-});
+fetch('navbar.html')
+.then(res => res.text())
+.then(text => {
+    let oldelem = document.querySelector("script#replace_with_navbar");
+    let newelem = document.createElement("div");
+    newelem.innerHTML = text;
+    oldelem.parentNode.replaceChild(newelem,oldelem)
+})
